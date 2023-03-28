6e2b602ea18d6ee278f857e5fc54a33e2c954dfc
AyimWilliam <odhisayim@gmail.com>
2023-03-28 15:25:08 +0300

    things are not working

--- index.js
+++ index.js
@@ -45,13 +45,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const ownernumber = document.querySelector("#ownernumber").value;
     const carimage = document.querySelector("#carimage").value;
     const carnumber = document.querySelector("#carnumber").value;
-    const addCarBtn = document.getElementById("addCarbtn");
-const formContainer = document.getElementById("form-container");
-
-addCarBtn.addEventListener("click", () => {
-  formContainer.style.display = "block";
-});
-
 
     const newCar = { carmodel, ownername, ownernumber, carimage, carnumber };
 
@@ -66,7 +59,7 @@ addCarBtn.addEventListener("click", () => {
       .then((car) => {
         const newCarElement = document.createElement("li");
         newCarElement.innerHTML = `
-          <span>${car.carmodel} ${car.ownername} ${car.ownernumber}  ${car.carimage} ${car.carnumber}</span>
+        
           
           <div class="car-details hidden">
             <p>: ${car.ownername}</p>
