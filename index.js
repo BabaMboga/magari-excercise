9ee6948aedf66dcc1e8efd9b48db2b81d7f74880
AyimWilliam <odhisayim@gmail.com>
2023-03-28 09:55:03 +0300

    json details presented on html

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ document.addEventListener('DOMContentLoaded',() => {
     const carList = document.querySelector('#car-list');
     const carDetails = document.querySelectorAll('#car-details');
 
-    fetch ('')
+
+// adds a list of owner names who are in the parking lot
+    fetch ('http://localhost:3000/cars')
     .then(response => response.json)
-    .then(cars)
+    .then(cars => {
+        cars.forEach(car => {
+            const li = document.createElement('li');
+            li.textContent = car.ownername;
+            li.addEventListener('click',() =>{
+                fetch(`http://localhost:3000/characters/${car.id}`)
+              .then(response => response.json())
+              .then(carDetails => {
+                
+                carDetails.innerHTML = `
+                  <img src="${carDetails.carimage}">
+                  <h2>${carDetails.carmodel}</h2>
+                  <p>'The owner number is'${carDetails.ownernumber} 'while the car number is' ${carDetails.carnumber}</p>
+                  <button id="Editbtn">Update</button>
+                  <button id="Delbtn">Check Out</button>
+                `;
+                carDetails.style.display = 'block';
+              });
+            })
+    })
 })
