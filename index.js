359ad70da6ced12673e70b38585ce6db9361a369
AyimWilliam <odhisayim@gmail.com>
2023-03-28 09:03:43 +0300

    Update

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded',() => {
-    
+    const carList = document.querySelector('#car-list');
+    const carDetails = document.querySelectorAll('#car-details');
+
+    fetch ('')
+    .then(response => response.json)
+    .then(cars)
 })
