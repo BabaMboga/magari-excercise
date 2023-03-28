ee343a8d511a031f39b0761a7b27c156b875d326
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-28 11:33:05 +0300

    update js and html

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-document.addEventListener('DOMContentLoaded',() => {
-    const carList = document.querySelector('#car-list');
-    const carDetails = document.querySelectorAll('#car-details');
+    const newCarForm = document.querySelector('#new-car-form');
+const carList = document.querySelector('#car-list');
 
-    fetch ('')
-    .then(response => response.json)
-    .then(cars)
-})
+newCarForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const make = document.querySelector('#make').value;
+  const model = document.querySelector('#model').value;
+  const year = document.querySelector('#year').value;
+  const color = document.querySelector('#color').value;
+
+  const newCar = { make, model, year, color };
+
+  fetch('http://localhost:3000/cars', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newCar)
+  })
+  .then(response => response.json())
+  .then(car => {
+    const newCarElement = document.createElement('li');
+    newCarElement.innerHTML = `
+      <span>${car.make} ${car.model} (${car.year}) - ${car.color}</span>
+      <button class="details-btn">Details</button>
+      <div class="car-details hidden">
+        <p>Make: ${car.make}</p>
+        <p>Model: ${car.model}</p>
+        <p>Year: ${car.year}</p>
+        <p>Color: ${car.color}</p>
+      </div>
+    `;
+    carList.appendChild(newCarElement);
+  });
+});
