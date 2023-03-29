007bd1f635843f7e4812c5f338e3d6e4816ce6f5
AyimWilliam <odhisayim@gmail.com>
2023-03-29 11:59:08 +0300

    Check out works

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const carList = document.querySelector("#car-list");
   const carDetails = document.querySelector("#car-details");
-  const addCarButton = document.querySelector('#addCarbtn');
-  const formContainer = document.getElementById('form-container');
-
-  addCarButton.addEventListener('click', () => {
-        formContainer.style.display = "block";
-});
-
+  const addCarButton = document.querySelector("#addCarbtn");
+  const formContainer = document.getElementById("form-container");
+  addCarButton.addEventListener("click", () => {
+    formContainer.style.display = "block";
+  });
+  
   // adds a list of owner names who are in the parking lot
-  fetch("http://localhost:3001/cars")
+  fetch("http://localhost:3000/cars")
     .then((response) => response.json())
     .then((cars) => {
       cars.forEach((car) => {
@@ -17,41 +16,65 @@ document.addEventListener("DOMContentLoaded", () => {
         li.textContent = car.ownername;
         li.addEventListener("click", (e) => {
           e.preventDefault();
-       let carID = car.id;
-       fetch(`http://localhost:3001/cars/${carID}`)
+          let carID = car.id;
+          fetch(`http://localhost:3000/cars/${carID}`)
             .then((response) => response.json())
             .then((carData) => {
-              console.log (carData)
+              console.log(carData);
               carDetails.innerHTML = `
                   <img src="${carData.carimage}">
                   <h2>${carData.carmodel}</h2>
-                  <p>The owner number is ${carData.ownernumber} while the car number is ${carDetails.carnumber}</p>
+                  <p>The owner number is ${carData.ownernumber} while the car number is ${carData.carnumber}</p>
                   <button id="Editbtn">Update</button>
                   <button id="Delbtn">Check Out</button>
-                `;
-            //  carsDetails.style.display = "block";
+              `;
+              const editBtn = document.querySelector("#Editbtn");
+              const delBtn = document.querySelector("#Delbtn");
+              editBtn.addEventListener("click", () => {
+                const updatedCar = {
+                  carmodel: "updated car model",
+                  ownername: "updated owner name",
+                  ownernumber: "updated owner number",
+                  carimage: "updated car image",
+                  carnumber: "updated car number"
+                };
+                fetch(`http://localhost:3000/cars/${carID}`, {
+                  method: "PATCH",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(updatedCar),
+                })
+                  .then((response) => response.json())
+                  .then((updatedData) => {
+                    console.log(updatedData);
+                  });
+              });
+              delBtn.addEventListener("click", () => {
+                fetch(`http://localhost:3000/cars/${carID}`, {
+                  method: "DELETE",
+                })
+                  .then((response) => response.json())
+                  .then((deletedData) => {
+                    console.log(deletedData);
+                  });
+              });
             });
         });
         carList.appendChild(li);
       });
     });
 
-  //   addCarButton.addEventListener('click',)
-
   const newCarForm = document.querySelector("#new-car-form");
-
   newCarForm.addEventListener("submit", (event) => {
     event.preventDefault();
-
     const carmodel = document.querySelector("#carmodel").value;
     const ownername = document.querySelector("#ownername").value;
     const ownernumber = document.querySelector("#ownernumber").value;
     const carimage = document.querySelector("#carimage").value;
     const carnumber = document.querySelector("#carnumber").value;
-
     const newCar = { carmodel, ownername, ownernumber, carimage, carnumber };
-
-    fetch("http://localhost:3001/cars", {
+    fetch("http://localhost:3000/cars", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -62,17 +85,17 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((car) => {
         const newCarElement = document.createElement("li");
         newCarElement.innerHTML = `
-        
-          
-          <div class="car-details hidden">
-            <p>: ${car.ownername}</p>
-            
-          </div>
-        `;
-        carList.appendChild(newCarElement);
-
-        // reset the form
-        newCarForm.reset();
-      });
+          <span>${car.carmodel}</span>
+          `;
+          carList.appendChild(newCarElement);
+          newCarForm.reset();
+          formContainer.style.display = "none";
+        });
+    });
   });
-});
+  
+  
+  
+  
+  
+  
